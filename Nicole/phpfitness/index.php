05272fe9019b56a3d79f281c605411216317f2cb
Nicole Saunders <nicostar26@yahoo.com>
2019-01-06 12:28:01 -0500

    Fitness website in php started

---
+++ Nicole/phpfitness/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/main.css">
+
+    <title>Fit Pros</title>
+</head>
+<body>
+    <div class="header">
+        <div class="nav">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Products</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+        <div class="logo">
+            <img src="img/fitnesslogo.png" alt="logo">
+        </div>
+    </div>
+
+    <!-- JUMBOTRON -->
+
+    <div class="jumbotron">
+        <div class="info">
+            <h1>Fitness Pros</h2>
+            <h3><i>Fitness From The Inside Out</i></h4>
+        </div>
+    </div>
+
+    <!-- PRODUCTS -->
+
+    <div class="products">
+        <div class="productimage">
+            <img src="img/supplement.gif" alt="supplement">
+        </div>
+        <div class="productdesc">
+            <h2>Products</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab laboriosam voluptatem reprehenderit iste sint id quasi quos dolorum pariatur! Odit odio voluptate sit voluptatum beatae quibusdam, reprehenderit tempora dolorum inventore atque dolore quis cumque earum nulla nostrum, aut perferendis? Dolores non dolorum aut nihil repellat accusantium provident ipsa. Neque quas expedita fugiat animi iure asperiores amet eligendi necessitatibus totam veniam, dignissimos quaerat doloribus. Expedita quasi voluptatibus dolore nihil tempora cupiditate provident, natus ea. Quasi, quae accusamus! Alias libero dicta repudiandae a, ipsam quia quae dolorum. Fuga sunt, quae laboriosam sit, cum eos voluptate suscipit pariatur error quidem mollitia aspernatur omnis.</p>
+        </div>
+    </div>
+
+    <!-- TRAINING -->
+<div class="container">
+    <div class="training">
+        <div class="trainingdesc">
+            <h2>Training</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur amet ipsum repellat pariatur ipsam sint accusantium et quisquam nisi iste. Excepturi animi consequuntur id modi sequi architecto ratione pariatur iste. Pariatur veniam deserunt ullam quo magnam cum nostrum nesciunt quos non, eligendi dolorem ab reprehenderit veritatis odit voluptate voluptates aspernatur assumenda dolores sit repellendus delectus, doloribus magni harum alias! Nihil delectus atque eum quibusdam dolore debitis veniam! Obcaecati voluptate amet eaque unde sint eligendi autem natus dolores, a cumque, tenetur delectus pariatur ipsam molestiae sit nulla aliquid rem velit nostrum fugit itaque excepturi ad ipsa impedit? Ullam dolor numquam corrupti.</p>
+        </div>
+        <div class="trainingimage">
+            <img src="img/training.gif" alt="training">
+        </div>
+        
+    </div>
+</div>
+
+<div class="footer">
+    <div class="address">
+        <h3>Fitness Name</h3>
+        <p>123 Anystreet ln, Anycity, AS 12345<br>123-456-7890</p>
+    </div>
+    <div class="signature">
+        <p>Copyright &copy;2019 | Site Design: <a href="http://nicolejsaunders.com">Nicole Saunders</a></p>
+    </div>
+</div>
+    
+</body>
+</html>
